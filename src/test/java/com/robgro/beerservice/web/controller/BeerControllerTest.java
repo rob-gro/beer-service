8fa68cf396b81646d7799df6fc67b7f8f45b7765
RobGro <r.grodzki74@gmail.com>
2022-04-14 20:33:13 +0100

    added JPA entity for Beer objects

--- src/test/java/com/robgro/beerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/robgro/beerservice/web/controller/BeerControllerTest.java
@@ -25,7 +25,7 @@ class BeerControllerTest {
     @Test
     void getBeerById() throws Exception {
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -47,7 +47,7 @@ class BeerControllerTest {
         BeerDto beerDto = BeerDto.builder().build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
-        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
